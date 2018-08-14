adc3c37d98fa5c9600a6f3eafe452355ed27f77e
maxxvalue <32428979+maxxvalue@users.noreply.github.com>
2018-08-15 01:19:26 +0700

    Update poke.php

--- poke.php
+++ poke.php
@@ -5,8 +5,8 @@ $access_token = 'euKTKZ17IAy2EuoIGL/jGkkbnHY7Uahp3N8Rosyg3SMx1G92EvZmwf0GHVWfMaq
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 require('function.php');
-// resetPoke();
-// resetMoney();
+ resetPoke();
+ resetMoney();
 if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		$replyToken = $event['replyToken'];
